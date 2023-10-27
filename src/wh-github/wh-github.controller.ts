5b3d7c6209fc1f50f35c388e0cab28ed3dfaf323
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-27 16:12:53 -0400

    Guard implementado

--- src/wh-github/wh-github.controller.ts
+++ src/wh-github/wh-github.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Headers, Body, Post } from '@nestjs/common';
+import { Controller, Headers, Body, Post, UseGuards } from '@nestjs/common';
 import { WhGithubService } from './wh-github.service';
 import { GitHubEvent, GitHubPayload } from 'src/interfaces/github.interface';
+import { GithubGuard } from 'src/github/github.guard';
 
 @Controller( 'github' )
 export class WhGithubController {
@@ -9,15 +10,16 @@ export class WhGithubController {
 
 
   @Post( '/' )
+  @UseGuards( GithubGuard )
   webhookHandler(
     @Headers( 'x-github-event' ) githubEvent: GitHubEvent,
     @Body() body: GitHubPayload,
   ): string {
     
     // console.log(body);
-    console.log({githubEvent});
+    // console.log({githubEvent});
     const json = JSON.stringify( body );
-    console.log(json);
+    // console.log(json);
 
     // console.log( { githubEvent, body } );
     this.whGithubService.handlePayload( githubEvent, body );
